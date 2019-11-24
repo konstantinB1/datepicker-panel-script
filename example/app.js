5d81331cd1d36f6466018fabd14a334b0c50d85d
Kontantin Bulatovic <konstantinbulatovic1@gmail.com>
2019-11-24 13:21:12 +0100

    Changing example to a class, removing utility class, changing philosophy from using project utils to third party ones

--- example/app.js
+++ example/app.js
@@ -1,50 +1,74 @@
-import { DatepickerScript } from '../lib/Datepicker.js'
+import DatepickerScript from '../lib/Datepicker.js'
 
-const datepicker = new DatepickerScript({
-  listener: el => {
-    console.log(el)
+class MyDatepicker {
+
+  /**
+   * Constructor
+   */
+  constructor () {
+    this.datepickerScript = new DatepickerScript({ listener: this.dayClickCallbackListener })
+
+    this.datepickerScript.computePanel()
+
+    this.initListeners()
+    this.render()
   }
-})
 
-const dpBody = document.querySelectorAll('.datepicker-body tr')
-const curPanel = datepicker.computePanel()
-
-function rerenderMonthPanel () {
-  const data = curPanel.formatMatrixGrid()
-  const len = data.length
-  const today = document.querySelector('.today')
-
-  if (today) {
-    today.classList.remove('today')
+  /**
+   * Day click instance
+   * 
+   * @param {DatepickerScriptDay} dayInstance 
+   */
+  dayClickCallbackListener (dayInstance) {
+    console.log(dayInstance)
   }
-  
-  document.querySelector('#date-info').innerHTML = `${datepicker.getMonthName} ${datepicker.getFullYear}`
 
-  for (let i = 0; i < len; i++) {
-    const row = data[i]
-    const getRowTr = dpBody[i]
+  /**
+   * Set listeners
+   */
+  initListeners () {
+    document.querySelector('#prev').addEventListener('click', () => {
+      this.datepickerScript.prevMonth()
+      this.render()
+    })
     
-    if (getRowTr) {
-      const getCells = getRowTr.querySelectorAll('td')
-      for (let x = 0; x < getCells.length; x++) {
-        const cell = getCells[x]
-        const dateData = row[x] || {}
-        dateData.addToListener(cell)
+    document.querySelector('#next').addEventListener('click', () => {
+      this.datepickerScript.nextMonth()
+      this.render()
+    })
+  }
 
-        cell.innerHTML = dateData.utils.getDay()
+  /**
+   * Render datepicker table
+   */
+  render () {
+    const datepicker = this.datepickerScript
+    const dpBody = document.querySelectorAll('.datepicker-body tr')
+    const data = datepicker.formatMatrixGrid()
+    const currentDate = datepicker.getCurrentDate
+    
+    document.querySelector('#date-info').innerHTML = moment(currentDate).format('MMMM - YYYY')
+  
+    for (let i = 0; i < data.length; i++) {
+      const row = data[i]
+      const getRowTr = dpBody[i]
+      
+      if (getRowTr) {
+        const getCells = getRowTr.querySelectorAll('td')
+  
+        for (let x = 0; x < getCells.length; x++) {
+          const cell = getCells[x]
+          const dateData = row[x] || {}
+
+          // Add elements to listener
+          dateData.addToListener(cell)
+  
+          cell.innerHTML = moment(dateData.date).format('DD')
+        }
       }
-    }
-  }  
+    }  
+  }
 }
 
-document.addEventListener('DOMContentLoaded', rerenderMonthPanel)
-
-document.querySelector('#prev').addEventListener('click', () => {
-  datepicker.prevMonth()
-  rerenderMonthPanel()
-})
-
-document.querySelector('#next').addEventListener('click', () => {
-  datepicker.nextMonth()
-  rerenderMonthPanel()
-})
+// Init
+document.addEventListener('DOMContentLoaded', () => new MyDatepicker())
